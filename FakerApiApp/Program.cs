e2644b4e09eb4144aeb8664fda2399356dc8d74e
Tookey <rachel.tookey@gmail.com>
2024-12-16 10:56:39 +0000

    First commit

---
+++ FakerApiApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FakerApiApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
